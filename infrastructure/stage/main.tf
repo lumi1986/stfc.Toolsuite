c82ce4bea54af10f8401722c46a77b526fa37a3a
lumi1986 <michael.luethi1986@gmail.com>
2024-06-02 21:02:17 +0200

    ci: init

---
+++ infrastructure/stage/main.tf
@@ -0,0 +1,10 @@
+resource "azurerm_resource_group" "stfc_toolsuite" {
+  name     = "STFC-Toolsuite"
+  location = var.resource_location
+}
+
+resource "azurerm_container_app_environment" "example" {
+  name                       = "Example-Environment"
+  location                   = azurerm_resource_group.stfc_toolsuite.location
+  resource_group_name        = azurerm_resource_group.stfc_toolsuite.name
+}
