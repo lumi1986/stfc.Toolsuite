b88e715465b09583432bdb2e63cf52852a67e540
lumi1986 <michael.luethi1986@gmail.com>
2024-06-03 06:39:12 +0200

    ci: example environment removed

--- infrastructure/stage/main.tf
+++ infrastructure/stage/main.tf
@@ -3,8 +3,8 @@ resource "azurerm_resource_group" "stfc_toolsuite" {
   location = var.resource_location
 }
 
-resource "azurerm_container_app_environment" "example" {
+/*resource "azurerm_container_app_environment" "example" {
   name                       = "Example-Environment"
   location                   = azurerm_resource_group.stfc_toolsuite.location
   resource_group_name        = azurerm_resource_group.stfc_toolsuite.name
-}
+}*/
