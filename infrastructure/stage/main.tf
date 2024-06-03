a04cddcb8d600e9c826ef7a97680af41471ed4a4
Michael Lüthi <153802108+lumi1986@users.noreply.github.com>
2024-06-03 07:14:52 +0200

    ci: add generic env (#1)

--- infrastructure/stage/main.tf
+++ infrastructure/stage/main.tf
@@ -3,8 +3,8 @@ resource "azurerm_resource_group" "stfc_toolsuite" {
   location = var.resource_location
 }
 
-/*resource "azurerm_container_app_environment" "example" {
-  name                       = "Example-Environment"
+resource "azurerm_container_app_environment" "this" {
+  name                       = "STFC-Toolsuite-Generic"
   location                   = azurerm_resource_group.stfc_toolsuite.location
   resource_group_name        = azurerm_resource_group.stfc_toolsuite.name
-}*/
+}
