13cad87ac3df13055faf6af4fb946bc563a36929
lumi1986 <michael.luethi1986@gmail.com>
2024-06-06 19:53:46 +0200

    ci: reformat and refactoring misspelled function

--- infrastructure/stage/main.tf
+++ infrastructure/stage/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "stfc_toolsuite" {
 }
 
 resource "azurerm_container_app_environment" "this" {
-  name                       = "STFC-Toolsuite-Generic"
-  location                   = azurerm_resource_group.stfc_toolsuite.location
-  resource_group_name        = azurerm_resource_group.stfc_toolsuite.name
+  name                = "STFC-Toolsuite-Generic"
+  location            = azurerm_resource_group.stfc_toolsuite.location
+  resource_group_name = azurerm_resource_group.stfc_toolsuite.name
 }
--- infrastructure/stage/providers.tf
+++ infrastructure/stage/providers.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.104.2"
     }
     tfe = {
-      source = "hashicorp/tfe"
+      source  = "hashicorp/tfe"
       version = "0.55.0"
     }
   }
@@ -15,11 +15,11 @@ provider "azurerm" {
   features {}
 
   skip_provider_registration = "true"
-  
+
   subscription_id = var.subscription_id
-  client_id = var.client_id
-  client_secret = var.client_secret
-  tenant_id = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
 provider "tfe" {
