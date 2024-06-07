7913ff3fdea724f7e48eac6790cd045a38759431
Michael Lüthi <153802108+lumi1986@users.noreply.github.com>
2024-06-07 07:34:27 +0200

    ci: add microservice repos under terraform control (#3)
    * ci: add microservice repos under terraform control
    
    * ci: adjust misspelled variable name
    
    * ci: typo on service name, import of existing repo

---
+++ infrastructure/stage/github.tf
@@ -0,0 +1,10 @@
+import {
+  to = github_repository.microservices["mitigation_calculator"]
+  id = "stfc.MitigationCalculator"
+}
+
+resource "github_repository" "microservices" {
+  for_each = var.microservices
+  name        = "stfc.${each.value.name}"
+  visibility = "public"
+}
