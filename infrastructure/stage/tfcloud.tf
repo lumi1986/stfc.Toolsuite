2b420ac4f78befafdd70070b6dae693e038c97d1
lumi1986 <michael.luethi1986@gmail.com>
2024-06-07 17:13:08 +0200

    ci: addressed wrong property

--- infrastructure/stage/tfcloud.tf
+++ infrastructure/stage/tfcloud.tf
@@ -25,7 +25,7 @@ resource "tfe_workspace" "microservice" {
   working_directory = "infrastructure/stage"
   vcs_repo {
     branch                     = "main"
-    identifier                 = "lumi1986/${github_repository.microservices[each.value.microservice_name].name}"
+    identifier                 = "lumi1986/${github_repository.microservices[each.value.microservice_key].name}"
     github_app_installation_id = var.terraform_cloud_github_app_id
   }
 }
