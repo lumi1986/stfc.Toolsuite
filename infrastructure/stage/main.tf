f5bc8ceecbc06faa755a6e698a4a136c1f57408a
lumi1986 <michael.luethi1986@gmail.com>
2024-06-21 12:29:05 +0200

    refactor: gitignore excluding local init and move variable location

--- infrastructure/stage/main.tf
+++ infrastructure/stage/main.tf
@@ -1,3 +1,21 @@
+locals {
+  workspaces = tomap({
+    for workspace in flatten([
+      for microservice_key, microservice in var.microservices : [
+        for environment_key, environment in var.environments : {
+          microservice_key           = microservice_key
+          environment_key            = environment_key
+          microservice_name          = microservice.name
+          microservice_friendly_name = microservice.friendly_name
+          environment_name           = environment.name
+          environment_friendly_name  = environment.friendly_name
+        }
+      ]
+    ])
+    : "${workspace.microservice_key}.${workspace.environment_key}" => workspace
+  })
+}
+
 resource "azurerm_resource_group" "stfc_toolsuite" {
   name     = "STFC-Toolsuite"
   location = var.resource_location
